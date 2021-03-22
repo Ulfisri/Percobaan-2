e83feb0d2fc3fcef5b0882535966fd4565847d9e
Ulfisri <81165803+Ulfisri@users.noreply.github.com>
2021-03-22 20:32:01 +0800

    Add files via upload

---
+++ Percobaan_2.ino
@@ -0,0 +1,210 @@
+const int a = 2;
+const int b = 3;
+const int c = 4;
+const int d = 5;
+const int e = 6;
+const int f = 7;
+const int g = 8;
+
+void setup (){
+  pinMode(a,OUTPUT);
+  pinMode(b,OUTPUT);
+  pinMode(c,OUTPUT);
+  pinMode(d,OUTPUT);
+  pinMode(e,OUTPUT);
+  pinMode(f,OUTPUT);
+  pinMode(g,OUTPUT);
+}
+
+void loop(){
+  //kosong
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(200);
+
+  //angka 1
+  digitalWrite(a,0);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(200);
+
+  //kosong
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(200);
+
+  //angka 5
+  digitalWrite(a,1);
+  digitalWrite(b,0);
+  digitalWrite(c,1);
+  digitalWrite(d,1);
+  digitalWrite(e,0);
+  digitalWrite(f,1);
+  digitalWrite(g,1);
+  delay(500);
+
+  //kosong
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(200);
+
+  //garis
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,1);
+  delay(500);
+
+  //kosong
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(200);
+
+  //angka 7
+  digitalWrite(a,1);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(500);
+
+  //Kosong
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(200);
+
+  //Garis
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,1);
+  delay(500);
+
+  //Kosong
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(200);
+
+  //Angka 2
+  digitalWrite(a,1);
+  digitalWrite(b,1);
+  digitalWrite(c,0);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,0);
+  digitalWrite(g,1);
+  delay(500);
+
+  //Kosong
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(200);
+
+  //Angka 0
+  digitalWrite(a,1);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,0);
+  delay(500);
+
+  //Kosong
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(200);
+
+  //Angka 0
+  digitalWrite(a,1);
+  digitalWrite(b,1);
+  digitalWrite(c,1);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,0);
+  delay(500);
+
+  //Kosong
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(200);
+
+  //Angka 2
+  digitalWrite(a,1);
+  digitalWrite(b,1);
+  digitalWrite(c,0);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,0);
+  digitalWrite(g,1);
+  delay(500);
+
+  //Kosong
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(700);
+
+}
